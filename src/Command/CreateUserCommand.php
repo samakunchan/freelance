3fcb88be1ee75110803eba7b7142c6f6f39a8bba
Samakunchan <samakunchan@gmail.com>
2018-09-21 01:56:30 +0200

    Command
    - Création d'une commande pour créer des utilisateurs.

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -2,39 +2,129 @@
 
 namespace App\Command;
 
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class CreateUserCommand extends Command
 {
-    protected static $defaultName = 'app:create-user';
+    protected static $defaultName = 'sama:create-user';
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * CreateUserCommand constructor.
+     * @param null $name
+     * @param ObjectManager $manager
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct($name = null, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    {
+        parent::__construct($name);
+        $this->manager = $manager;
+        $this->encoder = $encoder;
+    }
+
 
     protected function configure()
     {
-        $this
-            ->setDescription('Creation d\'un nouvel utilisateur')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        $this->setDescription('Creation d\'un nouvel utilisateur')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $helper = $this->getHelper('question');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $questionSurLeNom = new Question('Entrez votre nom et/ou prénom [Ex: PETIT Jean]: ');
+        $questionSurLeNom->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Vous devez entré en nom valide.'
+                );
+            }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+            return $answer;
+        });
+        $questionSurLeNom->setMaxAttempts(2);
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $questionSurLePseudo = new Question('Entrez votre pseudo: ');
+        $questionSurLePseudo->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Vous devez entré en nom valide.'
+                );
+            }
+
+            return $answer;
+        });
+        $questionSurLePseudo->setMaxAttempts(2);
+
+        $questionSurEmail = new Question('Entrez votre email: ');
+        $questionSurEmail->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Vous devez entré un email valide.'
+                );
+            }
+
+            return $answer;
+        });
+        $questionSurEmail->setMaxAttempts(2);
+
+        $questionSurLeMDP = new Question('Entrez votre mot de passe: ');
+        $questionSurLeMDP->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Vous devez entré en mot de passe valide.'
+                );
+            }
+
+            return $answer;
+        });
+        $questionSurLeMDP->setMaxAttempts(2);
+
+
+        $name = $helper->ask($input, $output, $questionSurLeNom);
+        $username = $helper->ask($input, $output, $questionSurLePseudo);
+        $email = $helper->ask($input, $output, $questionSurEmail);
+        $password = $helper->ask($input, $output, $questionSurLeMDP);
+
+        $user = new User();
+        $user->setFullname($name);
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $hash = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($hash);
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->manager->persist($user);
+        $this->manager->flush();
+        //$io = new SymfonyStyle($input, $output);
+        //$name = $input->getArgument('name');
+        //$password = $input->getArgument('password');
+//
+        //if ($name) {
+        //    $io->note(sprintf('Voici le nom: %s', $name));
+        //}
+//
+        //if ($password) {
+        //    $io->note(sprintf('Voici le mot de passe: %s', $password));
+        //}
+//
+        //$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
     }
 }
